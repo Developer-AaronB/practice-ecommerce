5a8e8dfcf8ba1c9ef91d234f0a42eb7c5d2626c8
Developer-AaronB <91814805+Developer-AaronB@users.noreply.github.com>
2022-09-26 17:04:09 -0500

    Update Products.ts

--- backend/schemas/Products.ts
+++ backend/schemas/Products.ts
@@ -1,5 +1,5 @@
-import { integer, select, text } from "@keystone-next/fields";
-import { list } from "@keystone-next/keystone/schema";
+import { integer, select, text } from '@keystone-next/fields';
+import { list } from '@keystone-next/keystone/schema';
 
 export const Product = list({
   // ToDo
@@ -8,23 +8,22 @@ export const Product = list({
     name: text({ isRequired: true }),
     description: text({
       ui: {
-        displayMode: "textarea",
+        displayMode: 'textarea',
       },
     }),
     status: select({
-        options: [
-            {label: 'Draft', value: 'DRAFT'},
-            {label: 'Available', value: 'AVAILABLE'},
-            {label: 'Unavailable', value: 'UNAVAILABLE'
-        },
-        ],
-        defaultValue: 'DRAFT',
-        ui: {
-            displayMode: 'segmented-control',
-            createView: { fieldMode: 'hidden' },
-        },
+      options: [
+        { label: 'Draft', value: 'DRAFT' },
+        { label: 'Available', value: 'AVAILABLE' },
+        { label: 'Unavailable', value: 'UNAVAILABLE' },
+      ],
+      defaultValue: 'DRAFT',
+      ui: {
+        displayMode: 'segmented-control',
+        createView: { fieldMode: 'hidden' },
+      },
     }),
     price: integer(),
-    //Todo: photo
+    // Todo: photo
   },
 });
