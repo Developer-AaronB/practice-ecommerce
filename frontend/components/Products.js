7df92b0eba2cc225880463e7bbdb15d4c9d0d5bf
Developer-AaronB <91814805+Developer-AaronB@users.noreply.github.com>
2022-09-27 00:03:48 -0500

    Products page with Query

---
+++ frontend/components/Products.js
@@ -0,0 +1,28 @@
+import gql from 'graphql-tag';
+import { useCallback } from 'react';
+
+const ALL_PRODUCTS_QUERY = gql`
+  query ALL_PRODUCTS_QUERY {
+    allProducts {
+      id
+      name
+      price
+      description
+      photo {
+        id
+        image {
+          publicUrlTransformed
+        }
+      }
+    }
+  }
+`;
+
+export default function Products() {
+  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
+  return (
+    <div>
+      <p>PRoducts!!</p>
+    </div>
+  );
+}
