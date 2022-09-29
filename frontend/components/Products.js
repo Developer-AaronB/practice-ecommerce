1030301cafa68e715e4a7cf1ec30447b6310eb36
Developer-AaronB <91814805+Developer-AaronB@users.noreply.github.com>
2022-09-29 09:54:44 -0500

    Product and Page components
    Fetching data with hooks and displaying it on our front end application.

--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -1,5 +1,8 @@
+import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
 import { useCallback } from 'react';
+import styled from 'styled-components';
+import Product from './Product';
 
 const ALL_PRODUCTS_QUERY = gql`
   query ALL_PRODUCTS_QUERY {
@@ -18,11 +21,23 @@ const ALL_PRODUCTS_QUERY = gql`
   }
 `;
 
+const ProductsListStyles = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+`;
+
 export default function Products() {
   const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
   return (
     <div>
-      <p>PRoducts!!</p>
+      <ProductsListStyles>
+        {data.allProducts.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </ProductsListStyles>
     </div>
   );
 }
